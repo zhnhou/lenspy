52180f2b424abe471fddc2dbe74b916d6a7fa410
Zhen Hou <zhn.hou@gmail.com>
2016-02-28 11:52:00 -0800

    minor edito

--- python/lenspy.py
+++ python/lenspy.py
@@ -7,3 +7,5 @@ class setup_lenspix_run(object):
     def __init__(self, workspace, Cls_file, nside=8192, lmax=10000, nsims=1, output_prefix='lensed_cmb', 
                  lens_method=1, pol=False, seed_init=0, output_unlensed=False, output_phimap=False):
         self.workspace = workspace
+        self.cls_file  = Cls_file
+        self.nside     = nside
